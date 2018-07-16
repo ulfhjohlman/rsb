209417c458abcc56dd40cd4de7220da455b425c1
Ulf <ulfhjohlman@gmail.com>
2018-07-16 23:00:37 +0200

    Initial Commit

---
+++ rsb/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  rsb
+//
+//  Created by Ulf Hjohlman on 2018-07-16.
+//  Copyright © 2018 Ulf Hjohlman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ rsb/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  rsb
+//
+//  Created by Ulf Hjohlman on 2018-07-16.
+//  Copyright © 2018 Ulf Hjohlman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
